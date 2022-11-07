2c46926f76b1d2bde6b2f20c9280ea7cef1857d8
Artom Pylypchuk <vallguard@gmail.com>
2022-11-07 14:28:15 +0200

    GetTwoNumbers with diplicates version

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -2,29 +2,27 @@ import java.util.Arrays;
 
 public class Program {
     public static void main(String[] args) {
-        int[] numbers = {3, 6, 7, 6, 7, 3, -4, 0, 3};
-        GetTwoMaxNumbers(numbers);
-
+        int[] numbers = {5, 3, 7, 3, 6, 2, 1,-5};
+        System.out.println(GetTwoMaxNumbers(numbers));
     }
 
-    public static String GetTwoMaxNumbers (int[] numbers) {
-        if(numbers.length == 0) {
-            throw new IllegalArgumentException("Array is null or empty");
-        }
-        int max = Integer.MIN_VALUE;
-        int secondMax = Integer.MIN_VALUE;
-        for (int number : numbers) {
-            if (number > max) {
-                secondMax = max;
-                max = number;
-            } else if (number > secondMax && number != max) {
-                secondMax = number;
+    public static String GetTwoMaxNumbers(int[] numbers) {
+        if (numbers.length > 1) {
+            int max = Integer.MIN_VALUE;
+            int secondMax = Integer.MIN_VALUE;
+            for (int number : numbers) {
+                if (number >= max) {
+                    secondMax = max;
+                    max = number;
+                } else if (number> secondMax) {
+                    secondMax = number;
+                }
             }
-        }
-        if(secondMax == Integer.MIN_VALUE) {
-            return "Array has only one maximum: " + max;
+            return "Array has two maximums: " + secondMax + ", " + max;
+        } else if (numbers.length == 1) {
+            return "Array has only one maximum: " + numbers[0];
         } else {
-            return  "Array has two maximums: " + secondMax + ", " + max;
+            return "Array is empty";
         }
     }
 }
